3777c71b7651e9f028fe65eeda76cbaf6c77739e
Subhojeet15 <subhojeetmitra540@gmail.com>
2024-09-11 22:47:52 +0530

    new commit

---
+++ questions/arrays/krotate.cpp
@@ -0,0 +1,42 @@
+/*
+K-Rotate
+Given an integer vector and a value k, your task is to rotate the array k times clockwise.
+
+Input Format
+
+In the function an integer vector and number k is passed.
+
+Output Format
+
+Return an integer vector.
+
+
+
+Sample Input
+
+{1, 3, 5, 7, 9}, x = 2
+
+
+Sample Output
+
+{7, 9, 1, 3, 5}
+
+
+Explanation
+
+After 1st rotation - {9, 1, 3, 5, 7}
+After 2nd rotation - {7, 9, 1, 3, 5}
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> kRotate(vector<int> a, int k){
+    // your code  goes here
+    int size=a.size();
+    vector<int> b(size);       //to avoid segmentation fault , declare the size of B-vector as the size of "a" vector
+    for(int i=0;i<a.size();i++)
+    {
+        b[(i+k)%size]=a[i];     //to clockwise inserting of elements into the "b" vector from "a" vector
+    }
+    return b;
+}
